09efd225d7da85f8d45b7a4b8611aa2c733482bb
Rwigema Herve <imenarwigema@gmail.com>
2026-03-09 10:48:18 +0200

    changing script running layout

--- setup.sh
+++ setup.sh
@@ -7,32 +7,46 @@ echo "========== Setting up VM =========="
 echo " --- Updating system ---"
 sudo apt-get update
 sudo apt-get upgrade -y
+echo
+echo
 
 # Install dependencies
 echo " --- Installing essential dependencies ---"
 sudo apt install -y git curl unzip build-essential vim
 git config --global core.editor vim
+echo
+echo
 
 # Installing Node and pnpm
 echo " --- Installing Node and pnpm ---"
 curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g pnpm pm2 
+echo
+echo
 
 # Installing Bun
 echo " --- Installing Bun ---"
 curl -fsSL https://bun.sh/install | bash
+echo
+echo
 
 # Install Nginx
 echo " --- Installing Nginx ---"
 sudo apt install -y nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
+echo
+echo
 
 # Install Solana Development tools (uncomment if needed)
 # curl --proto '=https' --tlsv1.2 -sSfL https://solana-install.solana.workers.dev | bash
-
+echo
 source ~/.bashrc
+echo
+echo
 
 echo "========== Done! =========="
-echo "Node: $(node -v) | pnpm: $(pnpm -v) | Nginx: $(nginx -v 2>&1)"
+echo "Node: $(node -v)"
+echo "pnpm: $(pnpm -v)"
+echo "Nginx: $(nginx -v 2>&1)"
